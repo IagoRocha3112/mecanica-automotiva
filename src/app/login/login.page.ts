68b1554911fd96d868d2eb93e06b3a26438f7ca7
IagoRocha3112 <iagorocha1992@gmail.com>
2020-11-16 22:45:34 -0300

    ticket #1

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  credentialsForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private menuCtrl: MenuController,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.menuCtrl.enable(false);
+    this.credentialsForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.credentialsForm.value);
+    this.router.navigate(['home']);
+  }
+
+}
